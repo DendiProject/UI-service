ce85c25efd76cf11cde3b8cd3204b9cdce4dae95
TsvetkovArtem <artem.tsvetkov.95@bk.ru>
2018-01-06 01:55:11 +0300

    I have fixed the menu at the top, made it beautiful, not on the buttons, this is the intermediate version. Later I'll do fine with the OOP.

--- src/main/java/com/netcracker/ui/service/UiServiceMainUI.java
+++ src/main/java/com/netcracker/ui/service/UiServiceMainUI.java
@@ -10,21 +10,20 @@ import com.jarektoro.responsivelayout.ResponsiveLayout;
 import com.jarektoro.responsivelayout.ResponsiveRow;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.FileResource;
-import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinService;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.Notification.Type;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -38,52 +37,79 @@ public class UiServiceMainUI extends UI {
     protected void init(VaadinRequest vaadinRequest) 
     {
         setSizeFull();//Пользовательский интерфейс на весь экран
+        /*try {
+            
+            CustomLayout cl = new CustomLayout(new ByteArrayInputStream("<div /><script>alert('Hi again!');</script>".getBytes()));
+        setContent(cl);
+        } catch (IOException ex) {
+            Logger.getLogger(UiServiceMainUI.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
         //Создание объектов 1 уровня
         ResponsiveLayout main_layout = new ResponsiveLayout();
         main_layout.setSizeFull();
         main_layout.setHeight("250%");
         setContent(main_layout);
         
-        ResponsiveRow offset_cap_from_the_top = main_layout.addRow();
+        
         ResponsiveRow navigate_row = main_layout.addRow();
         ResponsiveRow offset_contents_from_the_cap = main_layout.addRow();
         ResponsiveRow content_row = main_layout.addRow();
-        offset_cap_from_the_top.setHeight("6%");//Отступ шапки 
         offset_contents_from_the_cap.setHeight("6%");//Отступ контента от шапки
-        navigate_row.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        navigate_row.setDefaultComponentAlignment(Alignment.BOTTOM_CENTER);
         content_row.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-        
+
         //Создание объектов 2 уровня
-        //Объекты navigate_row
-        Button go_main_button = new Button("Main");
-        go_main_button.setSizeFull();
-        navigate_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(go_main_button);
+
+        /*CustomLayout  menu_layout1 = new CustomLayout("menulayout1");
+        navigate_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(menu_layout1);
         navigate_row.addColumn().withDisplayRules(1, 1, 1, 1);
         
-        Button recipes_button = new Button("Recipes");
-        recipes_button.setSizeFull();
-        navigate_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(recipes_button);
-        navigate_row.addColumn().withDisplayRules(4, 4, 4, 4);
+        CustomLayout  menu_layout2 = new CustomLayout("menulayout2");
+        navigate_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(menu_layout2);
+        navigate_row.addColumn().withDisplayRules(3, 3, 3, 3);
         
-        Button search_button = new Button("Search");
-        search_button.setSizeFull();
-        navigate_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(search_button);
+        CustomLayout  menu_layout3 = new CustomLayout("menulayout3");
+        navigate_row.addColumn().withDisplayRules(2, 2, 2, 2).withComponent(menu_layout3);
         navigate_row.addColumn().withDisplayRules(1, 1, 1, 1);
         
-        Button sign_in_button = new Button("Sign in");
-        sign_in_button.setSizeFull();
-        navigate_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(sign_in_button);
+        CustomLayout  menu_layout4 = new CustomLayout("menulayout4");
+        navigate_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(menu_layout4);*/
         
+        CustomLayout navigate_row_layout = new CustomLayout("NavigateRowLayout");
+        navigate_row.addColumn().withDisplayRules(12, 12, 12, 12).withComponent(navigate_row_layout);
+        ResponsiveLayout navigate_row_layout2 = new ResponsiveLayout();
+        navigate_row_layout.addComponent(navigate_row_layout2,"password");
+        ResponsiveRow menus_row = navigate_row_layout2.addRow();
+        menus_row.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        
+        CustomLayout  menu_layout1 = new CustomLayout("menulayout1");
+        menus_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(menu_layout1);
+        menus_row.addColumn().withDisplayRules(1, 1, 1, 1);
+        
+        CustomLayout  menu_layout2 = new CustomLayout("menulayout2");
+        menus_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(menu_layout2);
+        menus_row.addColumn().withDisplayRules(3, 3, 3, 3);
+        
+        CustomLayout  menu_layout3 = new CustomLayout("menulayout3");
+        menus_row.addColumn().withDisplayRules(2, 2, 2, 2).withComponent(menu_layout3);
+        menus_row.addColumn().withDisplayRules(1, 1, 1, 1);
+        
+        CustomLayout  menu_layout4 = new CustomLayout("menulayout4");
+        menus_row.addColumn().withDisplayRules(1, 1, 1, 1).withComponent(menu_layout4);
+        
+      
         
         //Объекты content_row
         ResponsiveLayout content_layout = new ResponsiveLayout();
         content_row.addColumn().withDisplayRules(10, 10, 10, 10).withComponent(content_layout);
         ResponsiveRow slider_row = content_layout.addRow();
-        Image sliders_image = new Image();
-        sliders_image.setSource(new FileResource(new File(VaadinService.getCurrent().getBaseDirectory().getAbsolutePath() + "/WEB-INF/images/slide1.png")));
-        sliders_image.setHeight("70%");
-        sliders_image.setWidth("100%");
-        slider_row.addColumn().withDisplayRules(12, 12, 12, 12).withComponent(sliders_image);
+        CustomLayout  slider_layout = new CustomLayout("sliderlayout");
+        //Image sliders_image = new Image();
+        //sliders_image.setSource(new FileResource(new File(VaadinService.getCurrent().getBaseDirectory().getAbsolutePath() + "/WEB-INF/images/slide1.png")));
+        //sliders_image.setHeight("70%");
+        //sliders_image.setWidth("100%");
+        //slider_row.addColumn().withDisplayRules(12, 12, 12, 12).withComponent(sliders_image);
+        slider_row.addColumn().withDisplayRules(12, 12, 12, 12).withComponent(slider_layout);
     
         
         
