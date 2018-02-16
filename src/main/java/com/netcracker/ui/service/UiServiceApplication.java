9d5b1eabec3d94d82b9538c72f041ba8d7a1c8f1
TsvetkovArtem <artem.tsvetkov.95@bk.ru>
2018-02-16 11:52:47 +0300

    add beans

--- src/main/java/com/netcracker/ui/service/UiServiceApplication.java
+++ src/main/java/com/netcracker/ui/service/UiServiceApplication.java
@@ -1,7 +1,13 @@
 package com.netcracker.ui.service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UiServiceApplication {
@@ -10,4 +16,18 @@ public class UiServiceApplication {
         SpringApplication.run(UiServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder)
+    {
+        return builder.build();
+    }
+     
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+        
+        return mapper;
+    }
 }
